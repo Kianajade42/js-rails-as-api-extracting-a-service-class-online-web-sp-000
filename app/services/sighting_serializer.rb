42f95519a5a10c43de5aff30724f90639e3cf615
Kiana Long <kianajade42.kl@gmail.com>
2021-04-19 15:58:40 +0000

    Automatically backed up by Learn

--- app/services/sighting_serializer.rb
+++ app/services/sighting_serializer.rb
@@ -6,6 +6,6 @@ class SightingSerializer
     @sighting.to_json(:include=>{:bird=>{:only=>[:name, :species]},
     :location=>{:only=>[:latitude,:longitude]}
     }, except=>[:updated_at])
-    
+
   end
 end
